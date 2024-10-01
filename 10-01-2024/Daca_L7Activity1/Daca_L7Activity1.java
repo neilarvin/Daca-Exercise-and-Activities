a575102ea13ad3293b50364cdc3270a33301499b
neilarvin <neil.daca@awsys-i.com>
2024-10-01 15:40:56 +0800

    L7 Activity 1 for October 1 2024

---
+++ 10-01-2024/Daca_L7Activity1/Daca_L7Activity1.java
@@ -0,0 +1,19 @@
+package Daca_L7Activity1;
+
+import java.util.Arrays;
+
+public class Daca_L7Activity1 {
+
+	public static void main(String[] args) {
+		
+		Passenger passenger = new Passenger("Neil Arvin Daca", 'M', "Filipino", "November 23, 1999");
+
+		System.out.println(passenger.name);
+		System.out.println(passenger.gender);
+		System.out.println(passenger.nationality);
+		System.out.println(passenger.dateOfBirth);
+		System.out.println(Arrays.toString(passenger.passport));
+		
+	}
+
+}
---
+++ 10-01-2024/Daca_L7Activity1/Passenger.java
@@ -0,0 +1,34 @@
+package Daca_L7Activity1;
+
+public class Passenger {
+	
+	public String name;
+	public char gender;
+	public String nationality;
+	public String dateOfBirth;
+	public String[] passport;
+	
+	public Passenger(String name, char gender, String nationality, String dateOfBirth) {
+
+		this.name = name;
+		this.gender = gender;
+		this.nationality = nationality;
+		this.dateOfBirth = dateOfBirth;
+		this.passport = new String[] {name, nationality, dateOfBirth};
+		
+	}
+	
+	
+//	Passenger(){
+//		
+//		name = "Neil Arvin Daca";
+//		gender = 'M';
+//		nationality = "Filipino";
+//		dateOfBirth = "November 23, 1999";
+//		passport = new String[] {name, String.valueOf(gender), nationality, dateOfBirth};
+//		
+//	}
+	
+	
+	
+}
